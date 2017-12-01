99daf48a2da421535bbc75c60da010a81b017d92
Użytkownik Gość <Guest@imac-18.local>
2017-12-01 10:02:28 +0100

    fresh project again

---
+++ zad1/zad1_sqlite/zad1_sqlite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zad1_sqlite
+//
+//  Created by Użytkownik Gość on 01.12.2017.
+//  Copyright © 2017 Waclawik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
