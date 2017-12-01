947f4b48a2fcb1bfb0619720368c64de63ceec8a
Użytkownik Gość <Guest@imac-18.local>
2017-12-01 09:54:55 +0100

    creating a project

---
+++ zad1/zad1_sqlite/zad1_sqlite/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  zad1_sqlite
+//
+//  Created by Użytkownik Gość on 01.12.2017.
+//  Copyright © 2017 Waclawik. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
